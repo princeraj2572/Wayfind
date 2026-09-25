85231b2d38384623cd55f0ee3bdf8092c64a3c91
Prince Raj <princeraj2572@gmail.com>
2026-09-25 12:58:36 +0530

    chore: polyfill jsdom gaps for Radix and scrolling

--- frontend/vitest.setup.ts
+++ frontend/vitest.setup.ts
@@ -2,4 +2,16 @@ import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
 
+if (typeof window !== "undefined") {
+  Element.prototype.hasPointerCapture ??= () => false;
+  Element.prototype.setPointerCapture ??= () => {};
+  Element.prototype.releasePointerCapture ??= () => {};
+  Element.prototype.scrollIntoView ??= () => {};
+  globalThis.ResizeObserver ??= class {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  } as unknown as typeof ResizeObserver;
+}
+
 afterEach(() => cleanup());
