931010830191516139791cb4bd74d9c53814a35c
Prince Raj <princeraj2572@gmail.com>
2026-09-25 13:15:08 +0530

    test: rename MSW lifecycle helper to setupMockServer

--- frontend/src/test/server.ts
+++ frontend/src/test/server.ts
@@ -3,7 +3,7 @@ import { setupServer } from "msw/node";
 export const server = setupServer();
 
 /** Call once at the top of a test file: starts MSW, fails on any unmocked request. */
-export function useMockServer() {
+export function setupMockServer() {
   beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
   afterEach(() => server.resetHandlers());
   afterAll(() => server.close());
