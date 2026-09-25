72acd91f32b77548a25bc74da514a73ae7068a97
Prince Raj <princeraj2572@gmail.com>
2026-09-25 13:06:50 +0530

    feat: add server-side proxy config

---
+++ frontend/src/lib/server/config.ts
@@ -0,0 +1,4 @@
+export const API_URL = (process.env.API_URL ?? "http://localhost:8000").replace(/\/$/, "");
+export const TOKEN_COOKIE = "wayfind_token";
+export const DEFAULT_MAX_AGE = 60 * 60;
+export const UPSTREAM_TIMEOUT_MS = 90_000;
